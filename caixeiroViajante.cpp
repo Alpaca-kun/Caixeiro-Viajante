7448641ef0889f0ee3af869e71f1c840ef049c26
Alpaca-kun <ra90558@uem.br>
2017-02-08 10:48:24 -0200

    Declaracao de funcoes

--- caixeiroViajante.cpp
+++ caixeiroViajante.cpp
@@ -1,65 +1,80 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 #include <valarray>
 
 using namespace std;
 //using namespace std::tr1;
 
-struct node {
+struct city {
   double xPos,
-        yPos;
+         yPos;
 };
 
-//typedef vector<double> tour;
-typedef valarray<node> tour;
+typedef vector<city> tour;
+//typedef valarray<node> tour;
 
-
-double calculateCities () {
-  
+double calculateCities (double x1, double x2, double y1, double y2) {
+    return sqrt(sqrt(abs(x2 - x1)) + sqrt(abs(y2 - y1)));
 }
 
-int calculateTour () {
-  
+double fitness () { //calculateTour
+  for (iterator it = )  
 }
 
-int selection () {
+double selection () {
  
 }
 
-int crossOver () {
+double crossOver () {
 
 }
 
-int mutation () {
+double mutation () {
 
 }
 
-int accepting () {
+double accepting () {
 
 }
 
-int replace () {
+double replace () {
 
 }
 
-int test () {
+double test () {
 
 }
 
 
 int main () {
-  int numVertex;
-  int xPosition,
-      yPosition;
+  int numVertex,
+      generations = 10000;
+      aux;
+
+  double xPosition,
+         yPosition;
 
   cin >> numVertex;
-
   for (int i = 0; i < numVertex; i++) {
+    cin >> aux;
+    cin.ignore();
     cin >> xPosition >> yPosition;
-    
-  }
-  
 
+    city = {xPosition, yPosition};
+    tour.push_back(city);
+  }
+
+  for (int i = 0; i < generations; i++) {
+    fitness();
+    //Create new population
+    selection();
+    crossOver();
+    mutation();
+    accepting();
+    replace();
+    test();
+  }   
 
   /* Passos
    * Fitness == Adequacao (re-calculate the values between cities)
